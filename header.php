11923f1ed814611d2aa8cac46b726e3d79914266
Randy Rehel <SmartiesGoldGaming@gmail.com>
2022-10-30 14:48:58 -0400

    premier commit du nouveau theme

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+   
+
+?>
+
+
+
+<section>
+     <div class="logo">
+        <img src="" alt="le logo du tim">
+    </div> 
+    <div class="nav">
+     <?php get_template_part( 'template-parts/components/nav');?>
+    </div>
+</section>
