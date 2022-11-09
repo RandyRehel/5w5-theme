1fbd81d5c9cc13582c9588eaec23be8ed5474a43
Randy Rehel <SmartiesGoldGaming@gmail.com>
2022-11-09 10:42:08 -0500

    FIX LA REDONDANCE DU CATEGORY PROFS !!!!! WOWWW

--- category-profs.php
+++ category-profs.php
@@ -1,20 +1,11 @@
  
- <?php
-    $args = array(
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'category_name' => 'profs',
-    'posts_per_page' => 20,
-    );
-    $arr_posts = new WP_Query( $args );
- ?>
     <?php get_header();?>
             <div class="pageContenuProfs" style="min-height:80vh;">
             <?php 
-                if ( $arr_posts->have_posts() ) :
+                if (have_posts()) :
   
-                    while ( $arr_posts->have_posts() ) :
-                        $arr_posts->the_post();
+                    while (have_posts() ) :
+                        the_post();
                         ?>
                         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                             <?php
@@ -28,8 +19,6 @@
                                     <h1 class="profTitre"><?php the_title(); ?></h1>
                                 </div>
                             </div>
-                            
-                            
                         </article>
                         <?php
                     endwhile;
