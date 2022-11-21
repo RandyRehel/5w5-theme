2a16be0044e8d5d9e2045b4a4245402e640fcf2d
Florence Lemieux Cayer <flolemc@gmail.com>
2022-11-21 15:10:29 -0500

    Header mobile

--- header.php
+++ header.php
@@ -82,6 +82,49 @@ elseif(!is_front_page()):?>
                 </div>
             
             
-        </div><!--Fin head -->
+        </div>
+        <header class="enterHeader mobile" x-data="{open: false}">
+            <button x-on:click="open = ! open"><svg viewBox="0 0 100 80" width="6vh" height="6vh">
+            <rect width="100" height="20"></rect>
+            <rect y="30" width="100" height="20"></rect>
+            <rect y="60" width="100" height="20"></rect>
+            </svg></button>
+            <div x-show="open">
+                <div class="nav enterNav">
+                <?php get_template_part( 'template-parts/components/nav');?>
+                    <div class="navImgDroit">
+                        <img class="timmien" src="<?=$img_dir?>Timien1.png" alt="timmien">
+                    </div>
+                    <div class="contactsHeader">
+                        <div class="leftHeader">
+                            <p>
+                                POUR NOUS COMMUNIQUER
+                            </p>
+                            <ul>
+                                <li><a><img class="icoHeader" src="<?=$img_dir?>IconeMail-1-1.png" alt="email"></a></li>
+                                <li><a><img class="icoHeader" src="<?=$img_dir?>InstaLogo-1.png_1.png" alt="instagram"></a></li>
+                                <li><a><img class="icoHeader" src="<?=$img_dir?>LogoFB.png" alt="facebook"></a></li>
+                            </ul>
+                        </div>
+                        <div class="rightHeader">
+                            <ul>
+                                <li><img src="<?=$img_dir?>LocationLogo-1.png.png" alt="emplacement"></li>
+                                <li><p class="texteEmplacement">COLLÈGE DE MAISONNEUVE<br>
+                                        3800, Rue SHERBROOKE EST <br>
+                                        MONTRÉAL (QUÉBEC) <br>
+                                        H1X 2A2 | (514) 254-7131</p></li>
+                            </ul>
+                        </div>
+                        
+                    </div>
+                    <div class="copyrightHead">
+                        <p>
+                            COPYRIGHT &copy; 2022 - TIM Maisonneuve. Tous droits réservés
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!--Fin head -->
 <?php endif?>
 
