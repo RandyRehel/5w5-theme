0c4f33722ca018316f8cf0e2513d59e4fb6453b2
Randy Rehel <SmartiesGoldGaming@gmail.com>
2022-12-05 17:42:08 -0500

    fix nav

--- template-parts/components/nav.php
+++ template-parts/components/nav.php
@@ -2,6 +2,29 @@
      $img_dir = get_theme_root_uri()."/../uploads/";
 ?>
 <ul>
-    
+    <a href="<?=$url?>/le-programme">
+        <li class="frontNavItems"> 
+            <p>Le Programme</p>
+            <img src="<?=$img_dir?>BoutonNav_hover-1.png" alt="image de cercles menus">
+        </li>
+    </a>
+    <a href="<?=$url?>/vie-etudiante">
+        <li class="frontNavItems"> 
+            <p>Vie étudiante</p>
+            <img src="<?=$img_dir?>BoutonNav_hover-1.png" alt="image de cercles menus">
+        </li>
+    </a>
+    <a href="<?=$url?>/category/projets">
+        <li class="frontNavItems"> 
+            <p>Projets</p>
+            <img src="<?=$img_dir?>BoutonNav_hover-1.png" alt="image de cercles menus">
+        </li>
+    </a>
+    <a href="<?=$url?>/stages">
+        <li class="frontNavItems"> 
+            <p>Stages</p>
+            <img src="<?=$img_dir?>BoutonNav_hover-1.png" alt="image de cercles menus">
+        </li>
+    </a>
 </ul>
 
